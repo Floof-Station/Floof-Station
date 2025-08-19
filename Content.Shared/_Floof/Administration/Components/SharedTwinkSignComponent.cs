6a48a6ba9d8965945b7db711ccbcb1f31459ce5a
Raikyr0 <Kurohana@hotmail.com.au>
2025-08-19 13:07:46 +1000

    Add files via upload

---
+++ Content.Shared/_Floof/Administration/Components/SharedTwinkSignComponent.cs
@@ -0,0 +1,8 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Floofstation.Administration.Components;
+
+[NetworkedComponent]
+public abstract partial class SharedTwinkSignComponent : Component
+{
+}
