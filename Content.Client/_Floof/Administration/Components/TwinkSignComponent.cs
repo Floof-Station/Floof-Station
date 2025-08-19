0515f2ff450daebd725d391a2ad1c4b14281d312
Raikyr0 <Kurohana@hotmail.com.au>
2025-08-19 13:15:05 +1000

    Add files via upload

---
+++ Content.Client/_Floof/Administration/Components/TwinkSignComponent.cs
@@ -0,0 +1,7 @@
+﻿using Content.Shared.Floofstation.Administration.Components;
+using Robust.Shared.GameStates;
+
+namespace Content.Client._Floof.Administration.Components;
+
+[RegisterComponent]
+public sealed partial class TwinkSignComponent : SharedTwinkSignComponent;
