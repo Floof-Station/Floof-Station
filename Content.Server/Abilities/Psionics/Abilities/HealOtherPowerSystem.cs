ef4237eed903f98d13c1ca9229013c3d200dc1a0
Raikyr0 <Kurohana@hotmail.com.au>
2025-11-21 19:41:17 +1100

    Makes it so Healing Word and Greater Healing Word can revive again

--- Content.Server/Abilities/Psionics/Abilities/HealOtherPowerSystem.cs
+++ Content.Server/Abilities/Psionics/Abilities/HealOtherPowerSystem.cs
@@ -133,7 +133,6 @@ public sealed class RevivifyPowerSystem : EntitySystem
             _damageable.TryChangeDamage(args.Target.Value, args.HealingAmount * args.ModifiedAmplification, true, false, damageableComponent, uid);
 
         if (!args.DoRevive
-            || HasComp<UnrevivableComponent>(args.Target.Value) // Floofstation - unrevivable
             || _rotting.IsRotten(args.Target.Value)
             || !TryComp<MobStateComponent>(args.Target.Value, out var mob)
             || !_mobState.IsDead(args.Target.Value, mob)
